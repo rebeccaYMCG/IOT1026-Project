8283db15fe214ed27c9a4603ba8ce9c3132afe32
A00241382@mycambrian.ca <123224843+rebeccaYMCG@users.noreply.github.com>
2023-06-15 16:22:48 -0400

    Create Pit Room Test

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -6,9 +6,31 @@ namespace MinotaurLabyrinthTest
     public class Tests
     {
         [TestMethod]
-        public void DummyTest()
+        public void PitRoomTest()
         {
-            Assert.AreNotSame(1, 2);
+            Pit pitRoom = new Pit();
+            Hero hero = new Hero();
+            Map map = new Map(1, 1);
+
+            pitRoom.Activate(hero, map);
+            Assert.AreEqual(pitRoom.IsActive, false);
+            Assert.AreEqual(hero.IsAlive, true);
+
+            hero.HasSword = true;
+            pitRoom.Activate(hero, map);
+            Assert.AreEqual(hero.IsAlive, true);
+
+            Pit newPitRoom = new Pit();
+            newPitRoom.Activate(hero, map);
+            Assert.AreEqual(hero.IsAlive, false);
+        }
+
+        [TestMethod]
+        public void GoblinGatheringTest()
+        {
+            GoblinGathering goblinGatheringRoom = new GoblinGathering();
+            Hero hero = new Hero();
+            Map map = new Map(1, 1);
         }
     }
 }
