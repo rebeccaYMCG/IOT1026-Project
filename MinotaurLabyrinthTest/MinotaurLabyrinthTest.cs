e99a0c37b0ea7f7d61e857ceae89c9be7713ed2e
A00241382@mycambrian.ca <123224843+rebeccaYMCG@users.noreply.github.com>
2023-06-23 00:36:55 -0400

    Create Gel Tests

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -74,5 +74,40 @@ namespace MinotaurLabyrinthTest
             minotaur.Activate(hero, map);
             Assert.AreEqual(hero.Location, new Location(2, 3));
         }
+
+        [TestMethod]
+        public void GelMoveTest()
+        {
+            Hero hero = new Hero();
+            var gelLocation = new Location(1, 1);
+            GelatinousCube gel = new(gelLocation);
+            Map map = new Map(4, 4);
+            map.GetRoomAtLocation(gelLocation).AddMonster(gel);
+            gel.Move(hero, map);
+
+            var expectedLocation = new Location(2, 1);
+            Assert.AreEqual(expectedLocation, gel.GetLocation());
+            gel.Move(hero, map);
+            expectedLocation = new Location(3, 1);
+            Assert.AreEqual(expectedLocation, gel.GetLocation());
+            gel.Move(hero, map);
+            expectedLocation = new Location(3, 1);
+            Assert.AreEqual(expectedLocation, gel.GetLocation());
+        }
+
+        [TestMethod]
+        public void GelMoveToActiveTest()
+        {
+            Hero hero = new Hero();
+            var gelLocation = new Location(1, 1);
+            GelatinousCube gel = new(gelLocation);
+            Map map = new Map(4, 4);
+            map.GetRoomAtLocation(gelLocation).AddMonster(gel);
+            map.SetRoomAtLocation(new Location(2, 1), RoomType.Pit);
+            gel.Move(hero, map);
+            // New location should be (1,1)
+            var expectedLocation = new Location(1, 1);
+            Assert.AreEqual(expectedLocation, gel.GetLocation());
+        }
     }
 }
